0d9063fb4b524ae5b0d14553df441ec93e95e7a1
Gabriel Cacayan <gabrielcacayan@gmail.com>
2021-07-04 17:46:36 +0800

    Fixed bug in mobile

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,8 +1,6 @@
 const hamOpen = document.getElementById("hamburger-open");
 const hamClose = document.getElementById("hamburger-close");
 const hamAct = document.getElementById("hamburger-active");
-const imgYowndrift = document.getElementById("img-yowndrift");
-const overlayYowndrift = document.getElementById("overlay-yowndrift");
 
 hamOpen.addEventListener("click", () => {
     hamAct.classList.toggle("nav-toggle");
@@ -11,8 +9,3 @@ hamOpen.addEventListener("click", () => {
 hamClose.addEventListener("click", () => {
     hamAct.classList.toggle("nav-toggle");
 });
-
-// imgYowndrift.addEventListener("mouseover", () => {
-//     overlayYowndrift.classList.toggle("overlay-yowndrift");
-//     // console.log("enter");
-// });
